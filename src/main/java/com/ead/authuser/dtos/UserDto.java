faa481bad33e62c259f9b1337c6cf913c2f76840
norma <normandojefferson@hotmail.com>
2025-08-03 09:51:57 -0300

    feat(JsonView): adicionar visões de Dto com JsonView e endpoints para atualizar password, senha e imagem

--- src/main/java/com/ead/authuser/dtos/UserDto.java
+++ src/main/java/com/ead/authuser/dtos/UserDto.java
@@ -1,22 +1,42 @@
 package com.ead.authuser.dtos;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Data;
-
-import java.util.UUID;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)  // Não exibe os campos null no JSON
 public class UserDto {
 
-    private UUID userId;
+    public interface UserView {
+        public static interface RegistrationPost {}
+        public static interface UserPut {}
+        public static interface PasswordPut {}
+        public static interface ImagePut {}
+    }
+
+    @JsonView(UserView.RegistrationPost.class)
     private String username;
+
+    @JsonView(UserView.RegistrationPost.class)
     private String email;
+
+    @JsonView({UserView.RegistrationPost.class, UserView.PasswordPut.class})
     private String password;
+
+    @JsonView(UserView.PasswordPut.class)
     private String oldPassword;
+
+    @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
     private String fullName;
+
+    @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
     private String phoneNumber;
+
+    @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
     private String cpf;
+
+    @JsonView(UserView.ImagePut.class)
     private String imageUrl;
 
 }
