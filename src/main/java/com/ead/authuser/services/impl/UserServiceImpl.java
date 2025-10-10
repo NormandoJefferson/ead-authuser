12298ac2fbf42e816521b03e7a905c4ff99f709c
norma <normandojefferson@hotmail.com>
2025-10-10 08:34:19 -0300

    feat(synchronous-communication): enviar delete para course após deletar um usuário

--- src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
+++ src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ead.authuser.services.impl;
 
+import com.ead.authuser.clients.CourseClient;
 import com.ead.authuser.models.UserCourseModel;
 import com.ead.authuser.models.UserModel;
 import com.ead.authuser.repositories.UserCourseRepository;
@@ -10,6 +11,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -23,6 +25,9 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserCourseRepository userCourseRepository;
+
+    @Autowired
+    CourseClient courseClient;
 
     @Override
     public List<UserModel> findAll() {
@@ -38,13 +43,24 @@ public class UserServiceImpl implements UserService {
         return userRepository.findById(userId);
     }
 
+    @Transactional
     @Override
     public void delete(UserModel userModel) {
+
+        boolean deleteUserCourseInCourse = false;
+
         List<UserCourseModel> userCourseModelList = userCourseRepository.findAllUserCourseIntoUser(userModel.getUserId());
         if (!userCourseModelList.isEmpty()) {
             userCourseRepository.deleteAll(userCourseModelList);
+            deleteUserCourseInCourse = true; // Se a lista não estiver vazia recebe true
         }
+
         userRepository.delete(userModel);
+
+        // Se for true manda o delete para o service de course
+        if (deleteUserCourseInCourse) {
+            courseClient.deleteUserInCourse(userModel.getUserId());
+        }
     }
 
     @Override
@@ -61,5 +77,7 @@ public class UserServiceImpl implements UserService {
     public boolean existsByEmail(String email) {
         return userRepository.existsByEmail(email);
     }
+
+
 
 }
